3760df287d30841ec044f2d3bf7a0bc5617d06cf
bekzod818 <bekzod030900@gmail.com>
2021-05-02 15:54:09 +0500

    static fayllar

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
-from .views import index, categories, archive
+from .views import index, categories, archive, about
 
 urlpatterns = [
     path('', index, name = 'home'), # http://127.0.0.1:8000/
+    path('about/', about, name = 'about'), # http://127.0.0.1:8000/about/
     path('cats/<slug:cat>/', categories, name = 'categories'), # http://127.0.0.1:8000/cats/1/
-    re_path(r'archive/(?P<year>[0-9]{4})/', archive), # http://127.0.0.1:8000/archive/2021/
+    # re_path(r'archive/(?P<year>[0-9]{4})/', archive), # http://127.0.0.1:8000/archive/2021/
 ]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from .models import *
+
+menu = ['Kategoriyalar' ,'Biz haqimizda', 'Bog\'lanish', "Ro'yhatdan o'tish"]
 
 def index(request):
-    return HttpResponse('<h1>Women app page</h1>')
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Asosiy sahifa'})
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'Biz haqimizda'})
 
 def categories(request, cat):
     if request.GET:
